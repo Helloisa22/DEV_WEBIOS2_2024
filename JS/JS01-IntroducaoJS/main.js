4b18eaf4f46d6c79bf59f06b6ceebcd17da255c3
Helloisa22 <helloisa.betriz44@gmail.com>
2024-10-10 20:28:02 -0300

    Aula js 01 finalizada

---
+++ JS/JS01-IntroducaoJS/main.js
@@ -0,0 +1,11 @@
+// Quando o usuario pesquisa algo no chatGPT chamamos isso de request
+
+// Quando o servidor responde chamamos isso de response
+
+
+// html --- <!-- -->
+// CSS --- /* */
+// JS --- // (uma linhda de codigo)
+// JS --- /* */ (Duas linhas ou mais de codigo)
+
+alert("Hello World / Olá Mundo JS")
