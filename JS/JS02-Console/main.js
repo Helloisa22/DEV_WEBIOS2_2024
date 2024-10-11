6a30743123fd9e923526492aefa1722acfc01eb3
Helloisa22 <helloisa.betriz44@gmail.com>
2024-10-10 21:43:45 -0300

    Aula js 02 nao finalizada

---
+++ JS/JS02-Console/main.js
@@ -0,0 +1,22 @@
+// quando eu quero criar uma variavel preciso inserir antes do nome dessa variavel as palavras reservadas var/let/const
+
+var teste = 0/0
+console.log(teste)
+
+var texto = "89"
+console.log(texto)
+console.clear()
+
+
+var primeiroNome = "Lukas"
+
+var segundoNome = " com K"
+
+
+var nomeAluno =  primeiroNome + segundoNome
+console.log(nomeAluno)
+
+
+var verdaira = true
+var falsa = false
+
