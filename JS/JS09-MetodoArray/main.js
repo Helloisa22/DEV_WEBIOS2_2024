4ce6e90b47cc89764d821b15a728a2b34b0f7889
Helloisa22 <helloisa.betriz44@gmail.com>
2024-11-08 20:51:27 -0300

    Aula JS 09 Finalizada

--- JS/JS09-MetodoArray/main.js
+++ JS/JS09-MetodoArray/main.js
@@ -6,14 +6,13 @@ let frutas = ["Pera", "Abacaxi", "Morango", "Melancia", "Macã", "melão", "lima
 let infoUsuario = [
     ["Ana", 1346], 
     ["Pedro", 2335], // 1
-    ["Caio", 566]
+    ["Caio", 566],
 ]
-
-// console.log(infoUsuario[1])
-
+// ForEach ele faz a função do for 
 infoUsuario.forEach((valor, indice) =>{
-    console.log(indice)
-    console
+    if(indice == 1){
+        console.log(valor)
+    }
 })
 
 
@@ -37,3 +36,64 @@ infoUsuario.forEach((valor, indice) =>{
 //     console.log(frutas[i]); // conteudo da posição
 //     console.log(i); // numero da posição
 // }
+
+
+console.clear()
+
+
+// array de valores aleatorios com 6 posiçoes > numeros
+
+// Math.currently / Math.max / Math.min / Math.sqrt
+let numeros = [4, 9, 16, 25, 78, 34]
+
+// Trazer um novo array atualizado com a raiz quadrado de cada número do array
+let numerosDois = numeros.map(Math.sqrt)
+let maiorNumero = Math.max.apply(Math, numeros)
+let menorNumero = Math.min.apply(Math, numeros)
+
+console.log(maiorNumero + " " + menorNumero)
+
+// console.log(numeros)
+// console.log(numerosDois)
+// console.log(maiorNumero)
+
+// console.clear()
+
+// Crie um array de 8 numero aleatorios > numerosTres
+
+let filtraNumero = numeros.filter((valor) => {
+    return valor >= 16
+})
+
+// criando um novo array onde ira conter apenas os número maiores ou iguais a 16
+console.log(numeros)
+console.log(filtraNumero)
+
+console.clear()
+
+// trabalhando com array de objeto
+
+let tarefa = [
+    {
+        tarefa: "Dormir",
+        isFeita: true
+    },
+    {
+        tarefa: "limpar",
+        isFeita: false
+    },
+    {
+        tarefa: "torcer",
+        isFeita: true
+    }
+]
+
+// retorne apenas as tarefas que estão marcadas como true
+// find
+// Busca*****
+let retornaTrue = tarefa.find((item) =>{
+    return item.isFeita === true;
+})
+
+console.log(retornaTrue)
+
