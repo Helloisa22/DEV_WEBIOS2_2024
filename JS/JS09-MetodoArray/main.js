e413b730e9df026d20ecbe7146501a5df34f2793
Helloisa22 <helloisa.betriz44@gmail.com>
2024-11-07 21:48:16 -0300

    JS 09 nao finalizada

---
+++ JS/JS09-MetodoArray/main.js
@@ -0,0 +1,39 @@
+// Array -- Metodo ForEach
+let frutas = ["Pera", "Abacaxi", "Morango", "Melancia", "Macã", "melão", "limao", "tomato"] // Coloque o nome de 8 frutas
+
+// nome usuario e pontuacao
+// Array bid
+let infoUsuario = [
+    ["Ana", 1346], 
+    ["Pedro", 2335], // 1
+    ["Caio", 566]
+]
+
+// console.log(infoUsuario[1])
+
+infoUsuario.forEach((valor, indice) =>{
+    console.log(indice)
+    console
+})
+
+
+
+// frutas.forEach((valor, indice) => {
+//      console.log(valor); // conteudo da posição
+//      console.log(indice); // numero da posição
+// })
+
+// frutas.forEach(Percorrer)
+///             conteudo, indice
+// function Percorrer(valor, indice){
+//     console.log(valor); // conteudo da posição
+//     console.log(indice); // numero da posição
+// }
+
+
+
+
+// for(i = 0; i <= frutas.length ; i++){
+//     console.log(frutas[i]); // conteudo da posição
+//     console.log(i); // numero da posição
+// }
