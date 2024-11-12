c74d7f55f4b36520dc5f81fb40151f8199f50556
Helloisa22 <helloisa.betriz44@gmail.com>
2024-11-12 19:52:03 -0300

    Aula js 10 finalizada

--- JS/JS10-Classes/main.js
+++ JS/JS10-Classes/main.js
@@ -9,9 +9,14 @@ class Aluno {
     }
 
     retornaIdade(){
-
+        let dataCompleta = new Date();
+        let pegaAno = dataCompleta.getFullYear()
+        return pegaAno - this.ano
     }
 }
+
+
+// classe no JS Date -- Retorna informações de data de acordo com o que está configurado no seu computador/servidor
 
 // Instancia == copias
 let aluno1 = new Aluno("Manasses", 1997, 1.90, true, true, "Black");
@@ -19,5 +24,6 @@ let aluno1 = new Aluno("Manasses", 1997, 1.90, true, true, "Black");
 let aluno2 = new Aluno("Jeferson", 2005, 1.75, true, true, "AllWhite")
 
 console.log(aluno1)
+console.log(`A idade do Manasses é ${aluno1.retornaIdade()}`)
 console.log(aluno2)
 
