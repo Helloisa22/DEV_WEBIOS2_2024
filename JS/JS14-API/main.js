c4ffdb28ff5005b1e1969d9831cb40b0ffc367ac
Helloisa22 <helloisa.betriz44@gmail.com>
2024-11-18 21:43:51 -0300

    Aula JS api semi finalizada

---
+++ JS/JS14-API/main.js
@@ -0,0 +1,13 @@
+let URL = 'https://dog.ceo/api/breeds/image/random'
+
+const DogApi = async () =>{
+    // Faço a consulta na API e retorno o JSON na variavel respo
+    let resp = await fetch(URL);
+
+    let exemplo = resp.json()
+    console.log(exemplo)
+
+}
+
+DogApi()
+
