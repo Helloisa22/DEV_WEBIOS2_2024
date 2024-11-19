75c21e44961c7560a45e5cff5f7ffcf6f86e9d30
Helloisa22 <helloisa.betriz44@gmail.com>
2024-11-19 20:13:48 -0300

    Aula JS 14 API finalizada

--- JS/JS14-API/main.js
+++ JS/JS14-API/main.js
@@ -2,12 +2,30 @@ let URL = 'https://dog.ceo/api/breeds/image/random'
 
 const DogApi = async () =>{
     // Faço a consulta na API e retorno o JSON na variavel respo
-    let resp = await fetch(URL);
+   await fetch(URL).then((response) => {
+        // Transformar o JSON recebido pelo FETCH em OBJ
+        return response.json();
+   }).then((response) => {
+        // Apresentar as informações para o Usuario
+        // Transformando novamente o OBJ em JSON
+        document.getElementById('mostraJSON').innerText = JSON.stringify(response);
 
-    let exemplo = resp.json()
-    console.log(exemplo)
-
+        let image = `
+            <img style="width: 200px; height:200px" src="${response.message}" alt="">
+        `
+        document.getElementById('mostraIMG').innerHTML = image;
+   })
 }
 
-DogApi()
 
+
+
+
+
+
+
+
+// '{"message":"https:\/\/images.dog.ceo\/breeds\/coonhound\/n02089078_1967.jpg","status":"success"}''
+
+// {message:"https:images.dog.ceo.breedscoonhound.n02089078_1967.jpg",
+// status:"success"}
